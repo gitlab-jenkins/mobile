6c687bd589088c6bde166f5a4b69d2673c2d8075
amir <amirh.sharafkar@gmail.com>
2016-09-17 18:41:15 +0430

    remove permissions

--- app/src/main/java/xyz/homapay/hampay/mobile/android/util/Constants.java
+++ app/src/main/java/xyz/homapay/hampay/mobile/android/util/Constants.java
@@ -113,6 +113,4 @@ public class Constants {
     //Permissions and Group Permission
     public static final int READ_PHONE_STATE = 1;
     public static final int READ_CONTACTS = 2;
-    public static final int ACCESS_FINE_LOCATION = 3;
-    public static final int GET_ACCOUNTS = 4;
 }
