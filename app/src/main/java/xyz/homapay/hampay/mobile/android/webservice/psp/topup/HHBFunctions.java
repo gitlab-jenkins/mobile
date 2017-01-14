26870a8eb958defdce4ac5dcbfc657389b372509
amir <amirh.sharafkar@gmail.com>
2017-01-14 16:10:20 +0330

    add topup token pay

---
+++ app/src/main/java/xyz/homapay/hampay/mobile/android/webservice/psp/topup/HHBFunctions.java
@@ -0,0 +1,31 @@
+package xyz.homapay.hampay.mobile.android.webservice.psp.topup;
+
+
+public class HHBFunctions
+{
+    public interface IFunc< Res>
+    {
+        Res Func() throws java.lang.Exception;
+    }
+
+    public interface IFunc1< T,Res>
+    {
+        Res Func(T p);
+    }
+
+    public interface IFunc2< T1,T2,Res>
+    {
+        Res Func(T1 p1,T2 p2);
+    }
+
+    public interface IAction
+    {
+        void Action();
+    }
+
+    public interface IAction1< T>
+    {
+        void Action(T p1);
+    }
+}
+
