9608573eb2467b6c6ef669e05619e3d62a37fe96
moahmmad <m.moradyar@homapay.com>
2017-01-30 11:29:50 +0330

    Start of friends invitation

---
+++ app/src/main/java/xyz/homapay/hampay/mobile/android/activity/ActivityFriendsInvitation.java
@@ -0,0 +1,19 @@
+package xyz.homapay.hampay.mobile.android.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import xyz.homapay.hampay.mobile.android.R;
+
+/**
+ * Created by mohammad on 1/30/17.
+ */
+
+public class ActivityFriendsInvitation extends ActivityParent {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends_invitation);
+    }
+}
